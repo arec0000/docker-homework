69ce57540ee605cdc01a25111c6cfe12c97ee504
Nikita <arec0000@gmail.com>
2023-10-03 11:03:00 +0300

    init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+interface Data {
+    data: {
+        attributes: {
+            title: string
+            description: string
+        }
+    },
+}
+
+export default async function Main() {
+    const res = await fetch(`http://${process.env.NEXT_PUBLIC_HOST}/api/data`, {
+        next: {
+            revalidate: 2
+        }
+    });
+
+    const { data } = await res.json() as Data;
+
+    return (
+        <div className="fixed left-[50%] top-[50%] bg-slate-300 min-w-[400px] translate-x-[-50%] translate-y-[-50%] p-5 rounded-3xl">
+            <h1 className="mb-2 text-xl">{data.attributes.title}</h1>
+            <p>{data.attributes.description}</p>
+        </div>
+    )
+}
